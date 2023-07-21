b6110084a9ea6db53f57adf192e787170e7b7fdf
Hemit Patel <itshp404@gmail.com>
2023-07-20 20:16:56 -0400

    added loading feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./App.css";
 
 const App = () => {
-  const [weather, setWeather] = useState()
-  const [name, setName] = useState()
-  const [feels_like, setFeelsLike] = useState()
-  const [humidity, setHumidity] = useState()
-  const api_key = '75d97d35ae146396198e702cd8a0165a'
+  const [weather, setWeather] = useState();
+  const [name, setName] = useState();
+  const [feels_like, setFeelsLike] = useState();
+  const [humidity, setHumidity] = useState();
+  const [loading, setLoading] = useState(true); // Add a loading state
+
+  const api_key = '75d97d35ae146396198e702cd8a0165a';
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
-       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=metric`)
-      .then((response) => {
-        setName(response.data.name)
-        setWeather(Math.floor(Math.round(response.data.main.temp)))
-        setFeelsLike(Math.floor(Math.round(response.data.main.feels_like)))
-        setHumidity(Math.floor(Math.round(response.data.main.humidity)))
-      })
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=metric`)
+        .then((response) => {
+          setName(response.data.name);
+          setWeather(Math.floor(Math.round(response.data.main.temp)));
+          setFeelsLike(Math.floor(Math.round(response.data.main.feels_like)));
+          setHumidity(Math.floor(Math.round(response.data.main.humidity)));
+          setLoading(false); // Set loading to false when data is fetched
+        })
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+          setLoading(false); // Set loading to false in case of an error
+        });
     });
-  }, [])
+  }, []);
+
+  // Show the loading screen while waiting for the API response
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className='weather_card'>
@@ -34,11 +46,11 @@ const App = () => {
         {weather} °C
       </p>
       {weather !== feels_like &&
-      <p className='weather_feelslike'>Feels like: {feels_like} °C</p>
+        <p className='weather_feelslike'>Feels like: {feels_like} °C</p>
       }
       <p className='weather_humidity'>Humidity: {humidity} %</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
